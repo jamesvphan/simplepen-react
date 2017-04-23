32f368fa693e3dbfda2a9e7f1f116932b8ca95ea
James Phan <jamesvphan@gmail.com>
2017-04-23 18:42:43 -0400

    Update note reducer to update a note

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -3,6 +3,7 @@ export default function noteReducer(state = {
   id: '',
   title: '',
   body: '',
+  notebook_id: ''
 }, action) {
   switch(action.type) {
     case 'LOAD_NOTE':
@@ -10,6 +11,7 @@ export default function noteReducer(state = {
       id: action.note.id,
       title: action.note.title,
       body: action.note.body,
+      notebook_id: action.note.notebook_id
     })
 
     default:
