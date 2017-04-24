1e52aca48d62b8c0e26168dba6258527c2d04d0f
dgwp-projects <zayneabraham1@gmail.com>
2017-04-24 14:51:09 -0400

    fixing layout

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -49,32 +49,33 @@ class Toolbar extends Component {
 	render(){
 		return(
 			<div className="ui">
-        <div className="wrapper">
-  				<div className="top editing">
-  					<button className="fullscreen useicons" title="Toggle fullscreen">
-              <span className="glyphicon glyphicon-fullscreen"></span>
-            </button>
+        		<div className="wrapper">
+  					<div className="top editing">
+  						
+  						<button className="fullscreen useicons" title="Toggle fullscreen">
+             	 			<span className="glyphicon glyphicon-fullscreen"></span>
+            			</button>
 
-            <button className="fullscreen useicons" title="Toggle fullscreen">
-              <span className="glyphicon glyphicon-link"></span>
-            </button>
+			            <button className="fullscreen useicons" title="Toggle fullscreen">
+			              <span className="glyphicon glyphicon-link"></span>
+			            </button>
 
-  					<button className="color-flip useicons" title="Invert colors" onClick={this.handleOnBold}>
-              <span className="glyphicon glyphicon-bold"></span>
-            </button>
+  						<button className="color-flip useicons" title="Invert colors" onClick={this.handleOnBold}>
+              				<span className="glyphicon glyphicon-bold"></span>
+            			</button>
 
-  					<button className="target useicons" title="Set target word count" onClick={this.handleOnItalics}>
-              <span className="glyphicon glyphicon-italic"></span>
-            </button>
+  						<button className="target useicons" title="Set target word count" onClick={this.handleOnItalics}>
+              				<span className="glyphicon glyphicon-italic"></span>
+           	 			</button>
 
-  					<button onClick={this.handleSave} className="save useicons" title="Save Text">
-              <span className="glyphicon glyphicon-floppy-disk"></span>
-            </button>
-  				</div>
+  						<button onClick={this.handleSave} className="save useicons" title="Save Text">
+              				<span className="glyphicon glyphicon-floppy-disk"></span>
+            			</button>
+  					</div>
 
-  				<div className="bottom">
-  					<button className="about">?</button>
-  				</div>
+	  				<div className="bottom">
+	  					<button className="about">?</button>
+	  				</div>
 			  </div>
 			</div>
 		)
